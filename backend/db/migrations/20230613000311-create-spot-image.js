0ef819fc609618fd4036ec47db3773d02dbce122
Brandon <vang.brandon94@gmail.com>
2023-06-13 15:37:53 -0700

    Fixed some errors

--- backend/db/migrations/20230613000311-create-spot-image.js
+++ backend/db/migrations/20230613000311-create-spot-image.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Spots',
+          model: 'Spot',
           key: 'id',
         },
         onUpdate: 'CASCADE',
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'Spots',
+        model: 'Spot',
         key: 'id',
       },
       onUpdate: 'CASCADE',
