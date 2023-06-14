ff418b266e46135445c6746dff6bef52318f68d9
Brandon <vang.brandon94@gmail.com>
2023-06-14 10:04:07 -0700

    Redid reviewData

--- backend/db/seeders/20230613210233-reviewData.js
+++ backend/db/seeders/20230613210233-reviewData.js
@@ -3,32 +3,37 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-
-    // Fetch the primary key values for the users and spots
-    const user = await queryInterface.sequelize.query('SELECT id FROM Users;');
-    const spot = await queryInterface.sequelize.query('SELECT id FROM Spots;');
-
-    // Use the retrieved primary key values in the review seed data
-    const reviewData = [
+    const reviewSeedData = [
       {
-        userId: user[0][0].id,
-        spotId: spot[0][0].id,
+        userId: 1,
+        spotId: 1,
         review: "This was an awesome spot!",
         stars: 5,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-      // Add more review data if needed
+      {
+        userId: 2,
+        spotId: 1,
+        review: "Great experience. Highly recommended!",
+        stars: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        spotId: 2,
+        review: "Not the best spot, but it was decent.",
+        stars: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ];
 
-    // Insert the review data into the Reviews table
-    await queryInterface.bulkInsert('Reviews', reviewData);
-
-    // Update the auto-increment sequence for the Reviews table
-    await queryInterface.sequelize.query('UPDATE SQLITE_SEQUENCE SET SEQ=1 WHERE NAME="Reviews";');
+    await queryInterface.bulkInsert('Reviews', reviewSeedData, {});
   },
 
   async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('Reviews', null, {});
-  }
+  },
 };
