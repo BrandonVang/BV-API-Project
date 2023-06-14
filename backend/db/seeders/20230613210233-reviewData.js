93316f4bcb594a43680b2226e5dae6bbb7c86f91
Brandon <vang.brandon94@gmail.com>
2023-06-14 09:57:13 -0700

    Debugging p2

--- backend/db/seeders/20230613210233-reviewData.js
+++ backend/db/seeders/20230613210233-reviewData.js
@@ -5,14 +5,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
 
     // Fetch the primary key values for the users and spots
-    const users = await queryInterface.sequelize.query('SELECT id FROM Users;');
-    const spots = await queryInterface.sequelize.query('SELECT id FROM Spots;');
+    const user = await queryInterface.sequelize.query('SELECT id FROM Users;');
+    const spot = await queryInterface.sequelize.query('SELECT id FROM Spots;');
 
     // Use the retrieved primary key values in the review seed data
     const reviewData = [
       {
-        userId: users[0][0].id,
-        spotId: spots[0][0].id,
+        userId: user[0][0].id,
+        spotId: spot[0][0].id,
         review: "This was an awesome spot!",
         stars: 5,
         createdAt: new Date(),
