fbc89b56c1811d6378b54ba6b46e5e8d5c636cc0
Brandon <vang.brandon94@gmail.com>
2023-06-30 09:37:00 -0700

    Finished phase 2

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,6 +17,7 @@ const removeUser = () => {
     };
 };
 
+//login
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
     const response = await csrfFetch("/api/session", {
@@ -51,6 +52,25 @@ const sessionReducer = (state = initialState, action) => {
 
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+//sign up
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
