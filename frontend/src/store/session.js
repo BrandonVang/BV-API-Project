de9f08dad0031129487821fb061223ae711ce100
Brandon <vang.brandon94@gmail.com>
2023-06-30 10:37:47 -0700

    Finished Phase 3

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -76,4 +76,13 @@ export const signup = (user) => async (dispatch) => {
     return response;
 };
 
+//Logout
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
 export default sessionReducer;
